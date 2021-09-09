5820465c5eb09fc5662073a2fd365c8f7f9a89b2
jkaddu <jkaddu@yahoo.com>
2021-09-08 19:38:24 -0700

    added axios, react-router-dom and boot/reactstrap

--- src/App.js
+++ src/App.js
@@ -1,57 +1,117 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import { axiosWithAuth } from "./auth/axiosWithAuth";
+import Home from "./components/Home";
+import AddMovies from "./components/AddMovies";
+import Edit from "./components/Edit";
+import Land from "./components/Land";
+// import PrivateRoute from "./components/PrivateRoute";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {
+          id: 1,
+          name: ''
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    const endpoint = "https://top9-the2nd.herokuapp.com/api/movies";
+    axios
+      .get(endpoint, {
+        headers: { authorization: localStorage.getItem("token") },
+      })
+      .then((res) => {
+        this.setState({
+          movies: res.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  addMovie = (movie) => {
+    // created an authorization component with headers/baseURL that is passed into axiosWithAuth()
+    axiosWithAuth()
+      .post()
+      .then(() => {
+        const movieName = {
+          id: Date.now(),
+          name: movie.name
+        }
+        const newMovie = [...this.state.movies, movieName]
+        this.setState({
+         movies: newMovie
+        });
+        window.location.href = "/home";
+      })
+      .catch((error) => console.log(error));
+  };
+
+  updateMovie = (e, id, updatedMovie) => {
+    e.preventDefault();
+    const endpoint = `https://top9-the2nd.herokuapp.com/api/movies/${id}`;
+    
+    axios
+      .put(endpoint, updatedMovie, {
+        headers: { Authorization: localStorage.getItem("token") },
+      })
+      .then((res) => {
+        console.log(res)
+        window.location.href = "/home";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  deleteMovie = (id) => {
+    const endpoint = `https://top9-the2nd.herokuapp.com/api/movies/${id}`;
+    axios
+      .delete(endpoint, {
+        headers: { Authorization: localStorage.getItem("token") },
+      })
+      .then((response) => {
+        const movie = response.data;
+        this.setState({ movie }); 
+        window.location.href = "/home";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route exact path="/" component={Land} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/home" render={(props) => (<Home {...props} mount={this.componentDidMount} movies={this.state.movies} delete={this.deleteMovie} /> )} />
+          <Route path="/movie"render={(props) => ( <AddMovies {...props} addMovie={this.addMovie} /> )} />
+          <Route path="/edit/:id" render={(props) => ( <Edit {...props} movies={this.state.movies} updateMovie={this.updateMovie} value={this.state.name} /> )} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
