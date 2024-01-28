6b4a0a9696ced22c76ec45a6643f4779ccb8297e
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-01-27 21:17:28 -0800

    slightly changed register format

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Col, Form, FormGroup, Input, Navbar, Button, Label } from "reactstrap";
 import axios from "axios";
-import '../App.css'
+import "../App.css";
 
 class Signup extends React.Component {
   state = {
@@ -21,15 +21,18 @@ class Signup extends React.Component {
   };
 
   validate = () => {
+    const name = document.getElementById("name");
     const input1 = document.getElementById("input1");
     const input2 = document.getElementById("input2");
+    if (name.value === null || name.value === "") {
+      alert("Username can't be empty!");
+    }
     if (input1.value !== "" && input2 !== "") {
       if (input1.value === input2.value) {
         return true;
       }
     }
     alert("Passwords don't match");
-    return false;
   };
 
   register = (e) => {
@@ -39,8 +42,8 @@ class Signup extends React.Component {
         "https://top9-the2nd.herokuapp.com/api/auth/register",
         this.state.newUser
       )
-      .then((response) => {
-        localStorage.setItem("token", response.data.token);
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
         this.props.history.push("/login");
       })
       .catch((error) => {
@@ -51,59 +54,61 @@ class Signup extends React.Component {
 
   render() {
     return (
-      <div>
-          <Navbar className="landNav">
-            <h2>Tops</h2>
-            <Button className='landBtn' color="primary" href="/login">Login</Button>
-          </Navbar>
-       
-      
-        <div className="register">
-          <h1>Sign Up</h1>
-          <div className='regCard'>
-            <Form className="form" onSubmit={this.register}>
-              <Col>
-                <FormGroup>
-                  <Label></Label>
-                  <Input
-                    type="username"
-                    name="username"
-                    placeholder="Usenname"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                    />
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <Label></Label>
-                  <Input
-                    id="input1"
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    />
-                </FormGroup>
-              </Col>
-              <Col>
-                <FormGroup>
-                  <Label></Label>
-                  <Input
-                    id="input2"
-                    type="password"
-                    name="password"
-                    placeholder="Confrim Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    />
-                </FormGroup>
-              </Col>
+      <div className="register">
+        <Navbar className="landNav">
+          <h2>Tops</h2>
+          <Button className="landBtn" color="primary" href="/login">
+            Login
+          </Button>
+        </Navbar>
 
-              <Button color="primary">Submit</Button>
-            </Form>
-          </div>
+        <div className="regCard">
+          <h1>Sign Up</h1>
+          <Form className="form" onSubmit={this.register}>
+            <Col>
+              <FormGroup>
+                <Label></Label>
+                <Input
+                  id="name"
+                  type="username"
+                  name="username"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label></Label>
+                <Input
+                  id="input1"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label></Label>
+                <Input
+                  id="input2"
+                  type="password"
+                  name="password"
+                  placeholder="Confrim Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+            </Col>
+
+            <Button className="btn" color="primary">
+              Submit
+            </Button>
+          </Form>
         </div>
       </div>
     );
