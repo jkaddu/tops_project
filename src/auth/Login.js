3c67c9b8652590001dca126a0ba140f3661754cc
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-23 20:19:05 -0500

    refactored login

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,54 +1,73 @@
-import React from "react";
-import axios from "axios";
-import { Col, Form, FormGroup, Input, Navbar, Button, Label } from "reactstrap";
+import { useRef, useState, useEffect, useContext } from "react";
+// import AuthContext from "./context/AuthProvider";
+import axios from "../api/axios";
 import "./auth.css";
+const LOGIN_URL = "/auth";
 
-class Login extends React.Component {
-  state = {
-    user: {
-      username: "",
-      password: "",
-    },
-  };
+const Login = () => {
+  // const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
 
-  login = (e) => {
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    axios
-      .post("https://top9-the2nd.herokuapp.com/api/auth/login", this.state.user)
-      .then((response) => {
-        localStorage.setItem("token", response.data.token);
-        this.props.history.push("/home");
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Invalid Credentials");
+
+    try {
+      let data = JSON.stringify({ username: user, password: pwd });
+      console.log(data);
+      const response = await axios.post(LOGIN_URL, data, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
       });
+      console.log(response);
+      console.log(response.data.accessToken);
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {
+      if (!err?.response) {
+        console.log("No server Reponse");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing username or password");
+      } else if ((err.response.tatus = 403)) {
+        setErrMsg("Uanthoized");
+      } else {
+        setErrMsg("Login failed");
+      }
+
+      console.log(err.message);
+      console.log(err.stack);
+    }
   };
 
-  handleChange = (e) => {
-    this.setState({
-      user: {
-        ...this.state.user,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
-
-  render() {
-    return (
+  return (
+    <>
       <div className="loginCont">
         <nav className="landNav">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="navIcon"
+            className="navIcon"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0 1 18 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0 1 18 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 0 1 6 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5"
             />
           </svg>
@@ -58,45 +77,67 @@ class Login extends React.Component {
           </button>
         </nav>
 
-        <div className="loginCard">
-          <h2>TOPS</h2>
-          <Form className="form" onSubmit={this.login}>
-            <Col>
-              <FormGroup>
-                <Label></Label>
-                <Input
-                  type="text"
-                  name="username"
-                  placeholder="Username"
-                  value={this.state.user.username}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup>
-                <Label></Label>
-                <Input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.user.password}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
-
-            <button className="navBtn" type="submit">
-              LOGIN
-            </button>
-          </Form>
-          <a className="create" href="/register">
-            Create an account
-          </a>
-        </div>
+        {success ? (
+          <section>
+            <h1>Success!</h1>
+            <p className="line">
+              <span>
+                <a href="/login">Sign In</a>
+              </span>
+            </p>
+          </section>
+        ) : (
+          <div className="loginCard">
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <h2>TOPS</h2>
+            <form className="form" onSubmit={handleSubmit}>
+              <label htmlFor="username" className="logLabel">
+                Username:
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                ref={userRef}
+                autoComplete="username"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
+              <label htmlFor="password" className="logLabel">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+              <button className="navBtn" type="submit">
+                LOGIN
+              </button>
+            </form>
+            <p className="line">
+              Don't have an account?
+              <br />
+              <span>
+                <a href="/register">Sign Up</a>
+              </span>
+            </p>
+          </div>
+        )}
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default Login;
