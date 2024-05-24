5f5c3c600e616974eb3c3b3729d27edad694f178
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-23 20:20:16 -0500

    refactored registration

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,73 +1,102 @@
-import React from "react";
-import { Col, Form, FormGroup, Input, Navbar, Button, Label } from "reactstrap";
-import axios from "axios";
-import "./auth.css";
-import "../components/components.css";
+import { useRef, useState, useEffect } from "react";
+import axios from "../api/axios";
 
-class Signup extends React.Component {
-  state = {
-    newUser: {
-      username: "",
-      password: "",
-    },
-  };
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = "/register";
 
-  handleChange = (e) => {
-    this.setState({
-      newUser: {
-        ...this.state.newUser,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
+const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
 
-  validate = () => {
-    const name = document.getElementById("name");
-    const input1 = document.getElementById("input1");
-    const input2 = document.getElementById("input2");
-    if (name.value === null || name.value === "") {
-      alert("Username can't be empty!");
-    }
-    if (input1.value !== "" && input2 !== "") {
-      if (input1.value === input2.value) {
-        return true;
-      }
-    }
-    alert("Passwords don't match");
-  };
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-  register = (e) => {
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+  }, [user]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
+
+  /***********************************************/
+  /* handleSubmit */
+  /***********************************************/
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    axios
-      .post(
-        "https://top9-the2nd.herokuapp.com/api/auth/register",
-        this.state.newUser
-      )
-      .then((res) => {
-        localStorage.setItem("token", res.data.token);
-        this.props.history.push("/login");
-      })
-      .catch((error) => {
-        console.log(error);
-        alert(error);
+    // guard if hacker trys to enable button with JS
+    const v1 = USER_REGEX.test(user);
+    const v2 = PWD_REGEX.test(pwd);
+    if (!v1 || !v2) {
+      setErrMsg("Invalid Entry");
+      return;
+    }
+
+    try {
+      let data = JSON.stringify({ username: user, password: pwd });
+      console.log(data);
+      const response = await axios.post(REGISTER_URL, data, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
       });
+      console.log(response);
+
+      setSuccess(true);
+      // clears state and input fields
+      setUser("");
+      setPwd("");
+      setMatchPwd("");
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 409) {
+        setErrMsg("Username Taken");
+      } else {
+        setErrMsg("Registration Failed");
+      }
+      console.log(err.message);
+      console.log(err.stack);
+    }
   };
 
-  render() {
-    return (
+  return (
+    <>
       <div className="register">
         <nav className="landNav">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            class="navIcon"
+            className="navIcon"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0 1 18 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0 1 18 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 0 1 6 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5"
             />
           </svg>
@@ -76,56 +105,133 @@ class Signup extends React.Component {
             <a href="/login">Login</a>
           </button>
         </nav>
+        {success ? (
+          <section>
+            <h1>Success!</h1>
+            <p className="line">
+              <span>
+                <a href="/login">Sign In</a>
+              </span>
+            </p>
+          </section>
+        ) : (
+          <section>
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <h1>Register</h1>
+            <form className="form" onSubmit={handleSubmit}>
+              <label className="logLabel" htmlFor="username">
+                Username:
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                ref={userRef}
+                autoComplete="username"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+              />
+              <p
+                id="uidnote"
+                className={
+                  userFocus && user && !validName ? "instructions" : "offscreen"
+                }
+              >
+                4 to 24 characters.
+                <br />
+                Must begin with a letter.
+                <br />
+                Letters, numbers, underscores, hyphens allowed.
+              </p>
 
-        <div className="regCard">
-          <h1>Create account</h1>
-          <Form className="form" onSubmit={this.register}>
-            <Col>
-              <FormGroup>
-                <Label></Label>
-                <Input
-                  id="name"
-                  type="username"
-                  name="username"
-                  placeholder="Username"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup>
-                <Label></Label>
-                <Input
-                  id="input1"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup>
-                <Label></Label>
-                <Input
-                  id="input2"
-                  type="password"
-                  name="password"
-                  placeholder="Confrim Password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-            </Col>
+              <label className="logLabel" htmlFor="password">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                aria-invalid={validPwd ? "false" : "true"}
+                aria-describedby="pwdnote"
+                onFocus={() => setPwdFocus(true)}
+                onBlur={() => setPwdFocus(false)}
+              />
+              <p
+                id="pwdnote"
+                className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+              >
+                8 to 24 characters.
+                <br />
+                Must include uppercase and lowercase letters, a number and a
+                special character.
+                <br />
+                Allowed special characters:{" "}
+                <span aria-label="exclamation mark">!</span>{" "}
+                <span aria-label="at symbol">@</span>{" "}
+                <span aria-label="hashtag">#</span>{" "}
+                <span aria-label="dollar sign">$</span>{" "}
+                <span aria-label="percent">%</span>
+              </p>
 
-            <button className="navBtn">SUBMIT</button>
-          </Form>
-        </div>
+              <label className="logLabel" htmlFor="confirm_pwd">
+                Confirm Password:
+              </label>
+              <input
+                type="password"
+                id="confirm_pwd"
+                name="confirm_pwd"
+                autoComplete="confirm_pwd"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
+                required
+                aria-invalid={validMatch ? "false" : "true"}
+                aria-describedby="confirmnote"
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+              />
+              <p
+                id="confirmnote"
+                className={
+                  matchFocus && !validMatch ? "instructions" : "offscreen"
+                }
+              >
+                Must match the first password input field.
+              </p>
+
+              <button
+                className="navBtn"
+                disabled={!validName || !validPwd || !validMatch ? true : false}
+              >
+                Sign Up
+              </button>
+            </form>
+            <p className="line">
+              Already registered?
+              <br />
+              <span>
+                <a href="/login">Sign In</a>
+              </span>
+            </p>
+          </section>
+        )}
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
-export default Signup;
+export default Register;
