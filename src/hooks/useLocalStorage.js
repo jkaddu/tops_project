f32795a1d041c22ee7e745c984031833e0a06843
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-30 16:01:58 -0500

    expanded useLocalStorage file

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
 
+const getLocalValue = (key, initValue) => {
+  // Server Side React aka Next.js
+  if (typeof window === "undefined") return initValue;
+
+  // if a value is already stored
+  const localValue = JSON.parse(localStorage.getItem(key));
+  if (localValue) return localValue;
+
+  // return the result of a function
+  if (initValue instanceof Function) return initValue;
+
+  return initValue;
+};
+
 const useLocalStorage = (key, initValue) => {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initValue
-  );
+  const [value, setValue] = useState(() => getLocalValue(key, initValue));
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
